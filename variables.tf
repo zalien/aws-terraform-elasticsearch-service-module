58d2c25f238837ce049cc3c5394e05d1a037e93a
Sérgio Teixeira <sergiopintoteixeira@gmail.com>
2020-02-20 14:46:01 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,4 +6,11 @@ variable "subnet_ids" { type = list }
 variable "volume_size" {}
 variable "instance_type" {}
 variable "instance_count" {}
+
+variable "tags" {
+  type        = map
+  description = "Tags for redis nodes"
+  default     = {}
+}
+
 variable "zone_awareness_enabled" { default = false }
